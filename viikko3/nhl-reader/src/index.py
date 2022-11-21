7b00ad4238ec71cbbf17dfd07059e183152a90ea
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-21 13:19:18 +0200

    prob 1,2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,31 @@
+import requests
 from player import Player
-
+from datetime import datetime
+"""
+Tee ohjelma, joka listaa suomalaisten pelaajien tilastot. 
+"""
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    # print("JSON-muotoinen vastaus:")
+    # print(response)
+
+    players = []
+    nationality = 'FIN'
+    for player_dict in response:
+        if player_dict['nationality'] == nationality:
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
+            players.append(player)
+
+    print(f"Players from {nationality} {datetime.now()}\n")
+    for player in sorted(players, key=lambda player: player.points, reverse=True):
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, goals, assists):
         self.name = name
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+        self.points = goals+assists
+
     
     def __str__(self):
-        return self.name
+        return f"{self.name:21}{self.team} {self.goals:2} + {self.assists:2} = {self.points}"
