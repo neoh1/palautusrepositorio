84ee413ab962ad3441edb4e8dab225901da8fcfd
neoh <niklasharju@hotmail.com>
2022-11-25 14:59:54 -0500

    prob4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -72,8 +72,43 @@ class TestKauppa(unittest.TestCase):
 
     def test_koriin_tuote_varastossa_tuote_ei_varastossa_suorita_ostos(self):
         self.kauppa.lisaa_koriin(1)
-        self.kauppa.lisaa_koriin(3)
+        self.kauppa.lisaa_koriin(3)  # ei varastossa
         self.kauppa.tilimaksu("pekka", "12345")
         self.pankki_mock.tilisiirto.assert_called_with(
             "pekka", 42, "12345", self.kaupan_tili, 5
         )
+
+    def test_asiointi_nolla_hinnat(self):
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 42, "12345", self.kaupan_tili, 6
+        )
+
+    def test_uusi_viite_jokaiselle_maksutapahtumalle(self):
+        viitegen_mock = Mock(wraps=Viitegeneraattori())
+        store = Kauppa(self.varasto_mock, self.pankki_mock, viitegen_mock)
+        store.aloita_asiointi()
+        store.lisaa_koriin(1)
+        store.tilimaksu("pekka", "12345")
+        self.assertEqual(viitegen_mock.uusi.call_count, 1)
+
+        store.aloita_asiointi()
+        store.lisaa_koriin(2)
+        store.tilimaksu("esa", "11223")
+        self.assertEqual(viitegen_mock.uusi.call_count, 2)
+
+        store.aloita_asiointi()
+        store.lisaa_koriin(1)
+        store.tilimaksu("sami", "99999")
+        self.assertEqual(viitegen_mock.uusi.call_count, 3)
+
+    def test_poista_korista_palautuu_varastoon(self):
+        varasto_mock = Mock(wraps=Varasto())
+        store = Kauppa(varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+        store.aloita_asiointi()
+        store.lisaa_koriin(1)
+        store.poista_korista(1)
+        self.assertEqual(varasto_mock.saldo(1), 100)
