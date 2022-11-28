b4fcd9eb8af7a147a7af4863f36a06d7dd20c6b6
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-28 19:58:21 +0200

    4. kaksi eri tuotetta, kaksi tavaraa korissa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,16 +5,21 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.mehu = Tuote("Mehu", 5)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.mehu)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
